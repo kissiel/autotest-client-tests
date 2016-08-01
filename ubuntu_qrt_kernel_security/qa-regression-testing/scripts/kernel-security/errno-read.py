533e1123848c0a28bedf81bb4b0d3276ba662590
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2016-08-01 22:41:21 +0200

    fix weird IO in errno-read.py
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- ubuntu_qrt_kernel_security/qa-regression-testing/scripts/kernel-security/errno-read.py
+++ ubuntu_qrt_kernel_security/qa-regression-testing/scripts/kernel-security/errno-read.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import sys
 try:
-    file(sys.argv[1]).read(int(sys.argv[2]))
+    f = open(sys.argv[1])
+    f.read(int(sys.argv[2]))
+    f.close()
 except IOError as e:
     print("%s: %s" % (sys.argv[1], e.strerror), file=sys.stderr)
     sys.exit(e.errno)
